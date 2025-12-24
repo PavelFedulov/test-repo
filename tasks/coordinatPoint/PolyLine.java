4a7e429b1eff2e15886ba1dd4b0f04f948ac8a5c
Pavel <pashafedulov77@yandex.ru>
2025-12-24 23:12:37 +0400

    Added class PolyLine Updated class Start

---
+++ tasks/coordinatPoint/PolyLine.java
@@ -0,0 +1,44 @@
+public class PolyLine {
+    public Point[] points;
+
+    public PolyLine() {
+        this.points = new Point[0];
+    }
+
+    public PolyLine(Point[] points) {
+        this.points = points;
+    }
+
+    public Line[] getLines() {
+        if (points.length < 2) {
+            return new Line[0];
+        }
+
+        Line[] lines = new Line[points.length - 1];
+        for (int i = 0; i < points.length - 1; i++) {
+            lines[i] = new Line(points[i], points[i + 1]);
+        }
+        return lines;
+    }
+
+    public double getLength() {
+        double sum = 0;
+        for (Line line : getLines()) {
+            sum += line.getLength();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Линия [");
+        for (int i = 0; i < points.length; i++) {
+            sb.append(points[i]);
+            if (i < points.length - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
--- tasks/coordinatPoint/Start.java
+++ tasks/coordinatPoint/Start.java
@@ -1,26 +1,35 @@
 public class Start {
     public static void main(String[] args) {
-        Point point1 = new Point(1,3);
-        Point point2 = new Point(5,8);
-        Point point3 = new Point(10,11);
-        Point point4 = new Point(15, 19);
+        Point point1 = new Point(1,5);
+        Point point2 = new Point(2,8);
+        Point point3 = new Point(5,3);
+        Point point4 = new Point(8, 9);
 
-        Line line1 = new Line(point1, point2);
-        Line line2 = new Line(point3, point4);
-        Line line3 = new Line(line1.pointEnd, line2.pointStart);
+        Point[] points = {point1, point2, point3, point4};
+        PolyLine polyLine = new PolyLine(points);
 
-        System.out.println(line3);
+        double polyLength = polyLine.getLength();
+        System.out.println("Длина Ломаной: " + polyLength);
 
-        line3.pointStart.x = 7;
-        line3.pointStart.y = 9;
+        Line[] lines = polyLine.getLines();
 
-        line3.pointEnd.x = 12;
-        line3.pointEnd.y = 14;
+        double linesLength = 0;
+        for (Line line : lines) {
+            linesLength += line.getLength();
+        }
+        System.out.println("Длина массива линий: " + linesLength);
 
-        System.out.println(line3);
+        System.out.println("Длина ломаной и длина массива линий совпадают: " + (polyLength == linesLength));
 
-        double totalLength = line1.getLength() + line2.getLength() + line3.getLength();
+        System.out.println("Координаты ломаной линии: " + polyLine);
 
-        System.out.println(totalLength);
+        point2.x = 12;
+        point2.y = 8;
+
+        System.out.println("Координаты измененной ломаной линии: " + polyLine);
+
+        for (Line line : lines) {
+            System.out.println(line);
+        }
     }
 }
