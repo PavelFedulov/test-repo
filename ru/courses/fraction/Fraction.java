b258727a256dc339bb3abb675b2ccf2dc224a02d
Pavel <pashafedulov77@yandex.ru>
2026-01-27 18:00:40 +0400

    Fraction extends Number

--- ru/courses/fraction/Fraction.java
+++ ru/courses/fraction/Fraction.java
@@ -1,4 +1,4 @@
-public class Fraction {
+public class Fraction extends Number {
     private final int numerator;
     private final int denominator;
 
@@ -28,5 +28,25 @@ public class Fraction {
     @Override
     public String toString() {
         return numerator + "/" + denominator;
+    }
+
+    @Override
+    public int intValue() {
+        return numerator / denominator;
+    }
+
+    @Override
+    public long longValue() {
+        return (long) numerator / denominator;
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) numerator / denominator;
+    }
+
+    @Override
+    public double doubleValue() {
+        return (double) numerator / denominator;
     }
 }
