26958f97a7e857920389317b46329ce1885965b8
Pavel <pashafedulov77@yandex.ru>
2026-02-01 15:50:28 +0400

    Added override equals in class Point

--- ru/courses/main/Start.java
+++ ru/courses/main/Start.java
@@ -1,15 +1,12 @@
 public class Start {
     public static void main(String[] args) {
-        Point p1 = new Point(0,0);
-        Point p2 = new Point(1,1);
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(1,2);
+        Point p3 = p1.clone();
 
-        Line l1 = new Line(p1, p2);
-        Line l2 = new Line(p1.clone(), p2.clone());
-        Line l3 = l1.clone();
-
-        System.out.println(l1.equals(l2));
-        System.out.println(l1 == l3);
-        System.out.println(l1.equals(l3));
-        System.out.println(l3);
+        System.out.println(p1.equals(p2));
+        System.out.println(p1 == p3);
+        System.out.println(p1.equals(p3));
+        System.out.println(p3);
     }
 }
