d32133859840492fc37491817f89191e4ff252ee
Pavel <pashafedulov77@yandex.ru>
2026-02-20 19:44:58 +0400

    Fixed class Sum for Task 2

--- ru/courses/exceptionHandling/Sum.java
+++ ru/courses/exceptionHandling/Sum.java
@@ -2,14 +2,25 @@ public class Sum {
     public static void main(String[] args) {
         double sum = 0;
 
-        for (String arg : args){
-            try {
+        for (String arg : args) {
+            if (isDouble(arg)) {
                 sum += Double.parseDouble(arg);
-            } catch (NumberFormatException _) {
-
             }
         }
 
         System.out.println(sum);
+    }
+
+    private static boolean isDouble(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
